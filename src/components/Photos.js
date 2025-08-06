f8250363b78295fb64609828566719a5f7e2aad8
juansv2k2 <32632671+juansv2k2@users.noreply.github.com>
2025-08-06 11:07:25 -0300

    fixed jpegs

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -3,10 +3,10 @@ import React from "react";
 function Photos() {
   return (
     <div className="rightSide">
-      <img className="imgBody" src="img/Figure_5.jpeg" />
+      <img className="imgBody" src="img/Figure_5.jpg" />
       <img className="imgBody" src="img/Figure_20.jpg" />
       <img className="imgBody" src="img/Figure_21.jpg" />
-      <img className="imgBody" src="img/Figure_22.jpeg" />
+      <img className="imgBody" src="img/Figure_22.jpg" />
       <img className="imgBody" src="img/Figure_24.jpg" />
     </div>
   );
