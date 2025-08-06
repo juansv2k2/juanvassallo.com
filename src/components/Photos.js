0af176e899464eef81f07140978018d0267e16ec
juansv2k2 <32632671+juansv2k2@users.noreply.github.com>
2025-08-06 10:53:57 -0300

    deleted photos

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -3,19 +3,11 @@ import React from "react";
 function Photos() {
   return (
     <div className="rightSide">
-      <img
-        className="imgBody"
-        src="img/AE1EF663-7567-4F5E-874F-85857C7F0FFC_1_105_c.jpeg"
-      />
-      <img
-        className="imgBody"
-        src="img/248E0274-38B9-4727-B024-71566C2F2190_1_105_c.jpeg"
-      />
-      <img className="imgBody" src="img/juan_edited_edited.jpg" />
-      <img
-        className="imgBody"
-        src="img/31224930_952206774948132_5286944826858668032_n.jpg"
-      />
+      <img className="imgBody" src="img/Figure_5.jpeg" />
+      <img className="imgBody" src="img/Figure_20.jpg" />
+      <img className="imgBody" src="img/Figure_21.jpg" />
+      <img className="imgBody" src="img/Figure_22.jpeg" />
+      <img className="imgBody" src="img/Figure_24.jpg" />
     </div>
   );
 }
