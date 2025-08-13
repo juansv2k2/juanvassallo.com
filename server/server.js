bb500229381f2faa6ed0c1deb57f04bdd798330c
juansv2k2 <32632671+juansv2k2@users.noreply.github.com>
2025-08-14 00:44:59 +0200

    server updated to ES6

--- server/server.js
+++ server/server.js
@@ -11,19 +11,18 @@
 //     console.log("Server is up!");
 // });
 
-const express = require("express");
-const favicon = require("express-favicon");
-const path = require("path");
+import express, { static as serveStatic } from "express";
+import { join } from "path";
 const port = process.env.PORT || 3000;
 const app = express();
-app.use(favicon(__dirname + "/build/favicon.ico"));
+
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
+app.use(serveStatic(__dirname));
+app.use(serveStatic(join(__dirname, "build")));
 app.get("/ping", function (req, res) {
-    return res.send("pong");
+  return res.send("pong");
 });
 app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(join(__dirname, "build", "index.html"));
 });
 app.listen(port);
